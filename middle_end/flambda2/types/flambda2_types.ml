5d4090d6b82907e328d66bf50246ba02b9be110f
Keryan Didier <keryan.didier@ocamlpro.com>
2021-12-06 11:50:40 +0100

    Proper error on code loading failure

--- middle_end/flambda2/types/flambda2_types.ml
+++ middle_end/flambda2/types/flambda2_types.ml
@@ -84,7 +84,6 @@ let extract_symbol_approx env symbol find_code =
           | Ok function_type ->
             let code_id = Function_type.code_id function_type in
             let code_or_meta = find_code code_id in
-            (* CR vlaviron: Should we fail if [code] is [None] ? *)
             Closure_approximation (code_id, code_or_meta)
         end)
       | Variant { immediates = Unknown; blocks = _; is_unique = _ }
