c6cf2b5e7d2a949efb647cee65777dc3ff4d4579
Diana Kalinichenko <dkalinichenko@janestreet.com>
2024-11-14 15:56:02 -0500

    bytecode compilation

--- tools/dumpobj.ml
+++ tools/dumpobj.ml
@@ -78,8 +78,12 @@ let record_events orig evl =
 let same_custom x y =
   Nativeint.equal (Obj.raw_field x 0) (Obj.raw_field (Obj.repr y) 0)
 
+external is_null : Obj.t -> bool = "%is_null"
+
 let rec print_obj x =
-  if Obj.is_block x then begin
+  if is_null x then
+    printf "null"
+  else if Obj.is_block x then begin
     let tag = Obj.tag x in
     if tag = Obj.string_tag then
         printf "%S" (Obj.magic x : string)
